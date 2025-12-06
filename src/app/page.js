3db0fa0b758da247788b8e6e320b0839a53f38ce
vi0letchen <violethongjinchen@gmail.com>
2025-12-06 23:22:01 +1300

    style:paddings

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+    <div className="flex min-h-screen justify-start bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full flex-col justify-center py-32 px-16 bg-white dark:bg-black">
+
+        <div className="flex flex-col gap-6 items-start text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Hello, I'm Violet!
           </h1>
@@ -22,7 +15,7 @@ export default function Home() {
             projects and get to know more about me.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div className="flex flex-col sm:flex-row items-start gap-4 text-base font-medium ">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="/resume.pdf"
