4d01c581c9b2e410be73340f275d4e52de5c7a58
vi0letchen <violethongjinchen@gmail.com>
2026-01-07 23:46:27 +1300

    feat:experience section

--- src/app/page.js
+++ src/app/page.js
@@ -38,7 +38,21 @@ export default function Home() {
           </a>
         </div>
       {/* Experience */}
-      
+        <div className="flex flex-col gap-6">
+          <h2 className="text-2xl font-semibold leading-9 tracking-tight text-black dark:text-zinc-50">
+            Experience
+          </h2>
+          <div className="flex flex-col gap-4">
+            <div>
+              <h3 className="text-xl font-medium leading-7 text-zinc-900 dark:text-zinc-100">
+                Software Developer
+              </h3>
+              <p className="text-zinc-600 dark:text-zinc-400">
+                University of Auckland - April 2025 to October 2025
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
