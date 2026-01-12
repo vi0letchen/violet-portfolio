6f970f36fd420b4b8ba235caef331a62cc0bf48a
vi0letchen <violethongjinchen@gmail.com>
2026-01-12 15:07:35 +1300

    feat:github button added

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,15 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Contact Me
+            Contact Me!
+          </a>
+                              <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://github.com/vi0letchen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
           </a>
                     <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
