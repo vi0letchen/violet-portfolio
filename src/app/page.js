ec6210f0f984dd739c452a35ccb3f0d6a04382eb
vi0letchen <violethongjinchen@gmail.com>
2026-01-13 22:07:50 +1300

    docs:prettier script

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
             View Resume
           </a>
 
-                              <a
+          <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://github.com/vi0letchen"
             target="_blank"
@@ -37,7 +37,7 @@ export default function Home() {
           >
             Github
           </a>
-                    <a
+          <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://www.linkedin.com/in/hongjin-chen-103a46280/"
             target="_blank"
@@ -45,7 +45,7 @@ export default function Home() {
           >
             LinkedIn
           </a>
-                    <a
+          <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="mailto:violetchenbusiness@gmail.com"
             target="_blank"
