872e91b3b5c1c3bde2a2671225833212f7fd7c47
vi0letchen <violethongjinchen@gmail.com>
2026-01-27 21:46:22 +1300

    style:blur effect on nabar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
-    <nav className=" flex items-center justify-between fixed top-0 left-0 w-full h-15 bg-background dark:bg-background shadow-md z-50">
-      <h1 className="text-xl font-bold ml-30">&lt;VioletChen/&gt;</h1>
+    <nav className=" flex items-center justify-between fixed top-0 left-0 w-full h-15 bg-background/80 shadow-md backdrop-blur-lg z-50">
+      <h1 className="text-xl font-bold ml-30 text-title">&lt;VioletChen/&gt;</h1>
     </nav>
   );
 }
